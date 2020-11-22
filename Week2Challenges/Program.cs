78de4b2b89979f68b7068f6b211a108f350fd1a3
Rafael Rivera <rriveratechie@gmail.com>
2020-11-22 00:00:38 -0500

    first commit

---
+++ Week2Challenges/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2Challenges
+{
+    public abstract class Airplane
+    {
+        public string Name { get; set; }
+        public bool IsJet { get; set; }
+        public double MachSpeed { get; set; }
+
+    }
+   
+        
+    }
+}
